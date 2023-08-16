fd7cb6e464d7c40d4d59b107cda5c258ff7564cf
Maxi Ferreira <charca@gmail.com>
2023-08-15 17:49:43 -0700

    Deploy to Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import preact from '@astrojs/preact'
 
-import netlify from '@astrojs/netlify/functions'
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: 'server',
-  adapter: netlify(),
+  adapter: cloudflare(),
   experimental: {
     viewTransitions: true,
   },
