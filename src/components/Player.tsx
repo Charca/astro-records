53674a8486dbc14b8e5488c4b8cb921fb0b5535c
Rob Pataki <robert@robertpataki.com>
2023-09-12 13:29:29 +0100

    Improve markup and accessibility
    - Enhance visibility of keyboard focus indicator so it is consistent across browsers - including Safari
    - Use button elements in Tracklist component
    - Allow the user to play/pause a track using the TrackList item buttons
    - Apply role="region" with accessible name for Tracklist and Player components
    - Hide album covers and icons from screen readers as they are only decorative images
    - Improve accessible name of track list items (state agnostic)
    - Make the H1 element contents more meaningful on the album pages
    - Improve playback controls in the TrackList items
    - Improve playback controls in the Player component
    - Make sure links have accessible labels
    - Add meta description

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,6 +1,6 @@
-import { currentTrack, isPlaying } from './state'
+import { currentTrack, isPlaying } from "./state";
 
-import { useEffect, useState, useRef } from 'preact/hooks'
+import { useEffect, useState, useRef } from "preact/hooks";
 
 const PlayIcon = (
   <svg
@@ -8,14 +8,16 @@ const PlayIcon = (
     viewBox="0 0 24 24"
     fill="currentColor"
     class="w-10 h-10 sm:w-14 sm:h-14"
+    aria-hidden="true"
+    focusable="false"
   >
     <path
       fill-rule="evenodd"
       d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
       clip-rule="evenodd"
-    ></path>
+    />
   </svg>
-)
+);
 
 const PauseIcon = (
   <svg
@@ -23,6 +25,8 @@ const PauseIcon = (
     viewBox="0 0 24 24"
     fill="currentColor"
     class="w-10 h-10 sm:w-14 sm:h-14"
+    aria-hidden="true"
+    focusable="false"
   >
     <path
       fill-rule="evenodd"
@@ -30,97 +34,157 @@ const PauseIcon = (
       clip-rule="evenodd"
     />
   </svg>
-)
+);
 
 // This app doesn't have real songs, it only has a few songs
 // that we will play over and over as the user uses the app.
-const MAX_SONGS = 4
+const MAX_SONGS = 4;
 
 export default function Player() {
-  const audioPlayer = useRef<HTMLAudioElement>(null)
-  const progressRef = useRef(null)
-  const [songIndex, setSongIndex] = useState(4)
-  const [progress, setProgress] = useState(0)
+  const audioPlayer = useRef<HTMLAudioElement>(null);
+  const progressRef = useRef(null);
+  const [songIndex, setSongIndex] = useState(4);
+  const [progress, setProgress] = useState(0);
 
   if (currentTrack.value === null) {
-    return
+    return;
   }
 
-  const { title, artist, imageUrl } = currentTrack.value
+  const { title, artist, albumName, imageUrl } = currentTrack.value;
 
   function whilePlaying() {
     if (audioPlayer.current.duration) {
       const percentage =
-        (audioPlayer.current.currentTime * 100) / audioPlayer.current.duration
-      setProgress(percentage)
+        (audioPlayer.current.currentTime * 100) / audioPlayer.current.duration;
+      setProgress(percentage);
     }
-    progressRef.current = requestAnimationFrame(whilePlaying)
+    progressRef.current = requestAnimationFrame(whilePlaying);
   }
 
   useEffect(() => {
-    const newIndex = (songIndex % MAX_SONGS) + 1
-    audioPlayer.current.src = `/mp3/song${newIndex}.mp3`
-    audioPlayer.current.currentTime = 0
-    audioPlayer.current.play()
-    setSongIndex(newIndex)
-  }, [title])
+    const newIndex = (songIndex % MAX_SONGS) + 1;
+    audioPlayer.current.src = `/mp3/song${newIndex}.mp3`;
+    audioPlayer.current.currentTime = 0;
+    audioPlayer.current.play();
+    setSongIndex(newIndex);
+  }, [title]);
 
   useEffect(() => {
     if (isPlaying.value) {
-      audioPlayer.current?.play()
-      progressRef.current = requestAnimationFrame(whilePlaying)
+      audioPlayer.current?.play();
+      progressRef.current = requestAnimationFrame(whilePlaying);
     } else {
-      audioPlayer.current?.pause()
-      cancelAnimationFrame(progressRef.current)
+      audioPlayer.current?.pause();
+      cancelAnimationFrame(progressRef.current);
     }
-  }, [isPlaying.value])
+  }, [isPlaying.value]);
 
   useEffect(() => {
     if (progress >= 99.99) {
-      isPlaying.value = false
-      setProgress(0)
+      isPlaying.value = false;
+      setProgress(0);
     }
-  }, [progress])
+  }, [progress]);
 
   return (
-    <div class="fixed bottom-0 left-0 right-0 bg-gray-100">
+    <div
+      class="fixed bottom-0 left-0 right-0 bg-gray-100"
+      role="region"
+      aria-labelledby="audio-player-heading"
+    >
+      <h2 id="audio-player-heading" class="sr-only">
+        Audio player
+      </h2>
       <div class="flex-1 bg-gray-200 h-1.5 dark:bg-gray-700">
-        <div class="bg-pink-500 h-1.5" style={`width: ${progress}%`}></div>
+        <div
+          aria-orientation="horizontal"
+          role="slider"
+          aria-label="audio timeline"
+          aria-valuemin={0}
+          aria-valuemax={
+            audioPlayer.current && audioPlayer.current.duration
+              ? Math.floor(audioPlayer.current.duration)
+              : 0
+          }
+          aria-valuenow={
+            audioPlayer.current && audioPlayer.current.currentTime
+              ? Math.floor(audioPlayer.current.currentTime)
+              : 0
+          }
+          aria-valuetext={`${
+            audioPlayer.current && audioPlayer.current.currentTime
+              ? Math.floor(audioPlayer.current.currentTime)
+              : 0
+          } seconds`}
+          class="bg-pink-500 h-1.5"
+          style={`width: ${progress}%`}
+        ></div>
       </div>
       <div class="container mx-auto max-w-screen-lg px-3 py-2 sm:px-6 sm:py-4 flex items-center gap-5">
-        <img src={imageUrl} width="60" height="60" class="block rounded-md" />
+        <img
+          src={imageUrl}
+          alt={`${artist} - ${albumName} album cover`}
+          aria-hidden="true"
+          width="60"
+          height="60"
+          class="block rounded-md"
+        />
         <div class="flex-1 min-w-0">
-          <p class="text-xl font-medium overflow-hidden text-ellipsis whitespace-nowrap">
+          <div class="text-xl font-medium overflow-hidden text-ellipsis whitespace-nowrap">
             {title}
-          </p>
-          <p class="text-xl text-gray-500 overflow-hidden text-ellipsis whitespace-nowrap">
+          </div>
+          <div class="text-xl text-gray-500 overflow-hidden text-ellipsis whitespace-nowrap">
             {artist}
-          </p>
+          </div>
         </div>
         <audio ref={audioPlayer} src="/mp3/song1.mp3" />
         <div class="flex gap-6 items-center text-black">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="w-10 h-10 hidden sm:block"
+          <button
+            type="button"
+            class="focus:ring-2 focus:outline-none focus:ring-black"
+            onClick={() => {}}
           >
-            <path d="M9.195 18.44c1.25.713 2.805-.19 2.805-1.629v-2.34l6.945 3.968c1.25.714 2.805-.188 2.805-1.628V8.688c0-1.44-1.555-2.342-2.805-1.628L12 11.03v-2.34c0-1.44-1.555-2.343-2.805-1.629l-7.108 4.062c-1.26.72-1.26 2.536 0 3.256l7.108 4.061z"></path>
-          </svg>
-
-          <button onClick={() => (isPlaying.value = !isPlaying.value)}>
-            {isPlaying.value ? PauseIcon : PlayIcon}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-10 h-10 hidden sm:block"
+              aria-hidden="true"
+              focusable="false"
+            >
+              <path d="M9.195 18.44c1.25.713 2.805-.19 2.805-1.629v-2.34l6.945 3.968c1.25.714 2.805-.188 2.805-1.628V8.688c0-1.44-1.555-2.342-2.805-1.628L12 11.03v-2.34c0-1.44-1.555-2.343-2.805-1.629l-7.108 4.062c-1.26.72-1.26 2.536 0 3.256l7.108 4.061z" />
+            </svg>
+            <span class="sr-only">Previous track</span>
           </button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="w-10 h-10 hidden sm:block"
+
+          <button
+            type="button"
+            class="focus:ring-2 focus:outline-none focus:ring-black"
+            onClick={() => (isPlaying.value = !isPlaying.value)}
           >
-            <path d="M5.055 7.06c-1.25-.714-2.805.189-2.805 1.628v8.123c0 1.44 1.555 2.342 2.805 1.628L12 14.471v2.34c0 1.44 1.555 2.342 2.805 1.628l7.108-4.061c1.26-.72 1.26-2.536 0-3.256L14.805 7.06C13.555 6.346 12 7.25 12 8.688v2.34L5.055 7.06z"></path>
-          </svg>
+            {isPlaying.value ? PauseIcon : PlayIcon}
+            <span class="sr-only">{isPlaying.value ? "Pause" : "Play"}</span>
+          </button>
+
+          <button
+            type="button"
+            class="focus:ring-2 focus:outline-none focus:ring-black"
+            onClick={() => {}}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-10 h-10 hidden sm:block"
+              aria-hidden="true"
+              focusable="false"
+            >
+              <path d="M5.055 7.06c-1.25-.714-2.805.189-2.805 1.628v8.123c0 1.44 1.555 2.342 2.805 1.628L12 14.471v2.34c0 1.44 1.555 2.342 2.805 1.628l7.108-4.061c1.26-.72 1.26-2.536 0-3.256L14.805 7.06C13.555 6.346 12 7.25 12 8.688v2.34L5.055 7.06z" />
+            </svg>
+            <span class="sr-only">Next track</span>
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/components/Record.tsx
+++ src/components/Record.tsx
@@ -1,25 +1,28 @@
-import { isPlaying, currentTrack } from './state'
+import { isPlaying, currentTrack } from "./state";
 
 export default function Record({
   albumId,
+  artist,
   title,
   imageUrl,
 }: {
-  albumId: string
-  title: string
-  imageUrl: string
+  albumId: string;
+  artist: string;
+  title: string;
+  imageUrl: string;
 }) {
   const isPlayingCurrentRecord =
-    isPlaying.value && currentTrack.value.albumId === albumId
+    isPlaying.value && currentTrack.value.albumId === albumId;
   const className =
-    'absolute top-0 opacity-0 vynil-image vynil-animation-in' +
-    (isPlayingCurrentRecord ? '-spinning' : '')
+    "absolute top-0 opacity-0 vynil-image vynil-animation-in" +
+    (isPlayingCurrentRecord ? "-spinning" : "");
 
   return (
     <div class="relative shadow-xl mr-32 w-72 md:w-auto">
       <img
         src={imageUrl}
-        alt={title}
+        alt={`${artist} - ${title} album cover`}
+        aria-hidden="true"
         width="400"
         height="400"
         class="block rounded-md tag-album-cover relative z-10 bg-white"
@@ -27,11 +30,13 @@ export default function Record({
       />
       <img
         src="/vynil-lp.webp"
+        alt=""
         width="400"
         height="400"
         class={className}
         style={`view-transition-name: vinyl-${albumId};`}
+        aria-hidden="true"
       />
     </div>
-  )
+  );
 }
